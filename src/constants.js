a9aa61dffbdde8e2ceb666aa7623a1300f983334
Steve Johnson <steve21johnson@gmail.com>
2019-10-26 14:34:30 -0700

    api base hinge

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
-// export const API_BASE = 'http://localhost:3001';
-console.log(process.env)
-export const API_BASE = 'https://ancient-mountain-14662.herokuapp.com';
+export let API_BASE = 'http://localhost:3001';
+if (process.env === "production") {
+  API_BASE = 'https://ancient-mountain-14662.herokuapp.com';
+}
