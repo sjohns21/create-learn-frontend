81d9497e8264bd641657a76f001e2786b0013c60
Steve Johnson <steve21johnson@gmail.com>
2019-10-26 14:27:53 -0700

    log process

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
-export const API_BASE = 'http://localhost:3001';
-// export const API_BASE = 'https://ancient-mountain-14662.herokuapp.com';
+// export const API_BASE = 'http://localhost:3001';
+console.log(process.env)
+export const API_BASE = 'https://ancient-mountain-14662.herokuapp.com';
