76cad504f316b77c070c2b9d800a62a9b0122e00
Steve Johnson <steve21johnson@gmail.com>
2019-10-26 14:40:26 -0700

    .NODE_ENV

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
-let API_BASE = 'http://localhost:3001';
-if (process.env === "production") {
+export let API_BASE = 'http://localhost:3001';
+if (process.env.NODE_ENV === "production") {
   API_BASE = 'https://ancient-mountain-14662.herokuapp.com';
 }
-export {API_BASE}
